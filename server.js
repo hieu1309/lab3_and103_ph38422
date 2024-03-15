7eba14790043c998524a828af90c2218bd674db5
hieunvph38422 <hieunvph38422@fpt.edu.vn>
2024-03-15 20:15:35 +0700

    hieu

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const app = express();
+
+const port = 4000;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+const uri = 'mongodb+srv://admin2:hieu1309@cluster0.0n8qgpd.mongodb.net/md18308';
+const mongoose = require('mongoose');
+const carModel = require('./carModel');
+
+app.get('/', async (req, res) => {
+    await mongoose.connect(uri);
+    let cars = await carModel.find();
+    console.log(cars);
+    res.send(cars);
+});
+
+app.post('/add_xe', async (req, res) => {
+    await mongoose.connect(uri);
+    let car = req.body;
+    console.log(car)
+    let kq = await carModel.create(car);
+    console.log(kq);
+    let cars = await carModel.find();
+    res.send(cars);
+
+})
+
+app.get('/xoa/:id', async (req, res) => {
+    await mongoose.connect(uri);
+    let id = req.params.id;
+    console.log(id);
+    // xu ly loi khi id khong dung
+    await carModel.deleteOne({_id: id});
+    res.redirect('../')
+}) 
+
+app.get('/update/:ten', async (req, res) => {
+    await mongoose.connect(uri);
+    console.log('Ket noi DB thanh cong');
+    let tenXe = req.params.ten;
+    console.log(tenXe);
+    let tenXeMoi = tenXe + ' Phien ban moi 2024';
+    await carModel.updateOne({ten: tenXe}, {ten: tenXeMoi});
+    let xehois = await carModel.find({});
+    res.send(xehois);
+})
